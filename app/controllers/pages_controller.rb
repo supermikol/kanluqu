292c084339dc7faa298d36cbce01cc9d4791ecbf
supermikol <mdu123@gmail.com>
2016-05-09 11:29:35 -0700

    add admin index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,16 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_admin, only: [:admin_index]
 
   def index
   end
 
+  def admin_index
+  end
 
+  private
+
+    def authenticate_admin
+      redirect_to root_path unless current_user && current_user.admin?
+    end
 end
