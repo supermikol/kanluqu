33e806d56ad985523f08fd95738f4e4686b116ed
supermikol <mdu123@gmail.com>
2016-05-10 23:14:10 -0700

    add description column to EC

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510043430) do
+ActiveRecord::Schema.define(version: 20160511061116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160510043430) do
     t.date     "end_date"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.text     "description"
   end
 
   create_table "favorites", force: :cascade do |t|
