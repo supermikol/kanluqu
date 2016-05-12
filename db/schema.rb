40b7dd3ad2f5f8a3c107e13c4690a291bc581ad3
supermikol <mdu123@gmail.com>
2016-05-12 13:59:05 -0700

    add zone to application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512182913) do
+ActiveRecord::Schema.define(version: 20160512203010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160512182913) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.text     "note"
+    t.string   "zone"
   end
 
   add_index "applications", ["user_id"], name: "index_applications_on_user_id", using: :btree
