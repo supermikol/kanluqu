ac5db40b3c3c9b9898b9f66617d6b11d37e8421a
supermikol <mdu123@gmail.com>
2016-05-12 11:50:40 -0700

    modify button

--- db/migrate/20160512182913_add_note_to_applications.rb
+++ db/migrate/20160512182913_add_note_to_applications.rb
@@ -1,5 +1,5 @@
 class AddNoteToApplications < ActiveRecord::Migration
   def change
-    add_column :applications, :note, :string
+    add_column :applications, :note, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511061116) do
+ActiveRecord::Schema.define(version: 20160512182913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160511061116) do
     t.boolean  "study_abroad"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "note"
   end
 
   add_index "applications", ["user_id"], name: "index_applications_on_user_id", using: :btree
