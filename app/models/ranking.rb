9c645067697aead73ce82b3a41cb103953032dd8
supermikol <mdu123@gmail.com>
2016-05-22 19:53:31 -0700

    fix validations

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,4 +1,4 @@
 class Ranking < ActiveRecord::Base
   belongs_to :school
-  validates :rank, uniqueness: { scope: :year, message: "Cannot have multiple ranks for a single year."}
+  validates :school, uniqueness: { scope: :year, message: "Cannot have multiple ranks for a single year."}
 end
