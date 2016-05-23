ec0237aeb5ef34266eabf69fd448a718a413fec4
supermikol <mdu123@gmail.com>
2016-05-22 19:57:14 -0700

    fix validations2

--- app/models/test_score.rb
+++ app/models/test_score.rb
@@ -1,5 +1,4 @@
 class TestScore < ActiveRecord::Base
   belongs_to :application
   validates :category, :subject, :score, presence: true
-  validates :subject, uniqueness: { scope: :category, message: "Please report only the highest score for a single subject."}
 end
